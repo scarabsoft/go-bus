c1518f38c0e607f3764c747db45e5df0f957035e
Dominique Dymke <dominique@dymke.net>
2021-07-12 21:01:45 +0700

    export bus interface

--- exported.go
+++ exported.go
@@ -9,6 +9,20 @@ import (
 	"sync"
 )
 
+type Bus interface {
+	Publish(name string, payloads ...interface{}) error
+
+	Subscribe(name string, handlers ...func(ID uint64, name string, payload interface{})) error
+
+	Unsubscribe(name string, handlers ...func(ID uint64, name string, payload interface{})) error
+
+	CreateTopic(name string, tb topic.Builder) (topic.Topic, error)
+
+	Get(name string) (topic.Topic, error)
+
+	DeleteTopic(name string) error
+}
+
 type Event struct {
 	ID      uint64
 	Topic   string
